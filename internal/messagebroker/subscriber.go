bf4129b871d9567427690bc3083b4c863483cd12
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2024-08-16 11:54:33 +0200

    [Feat.] Add error check for notify

--- internal/messagebroker/subscriber.go
+++ internal/messagebroker/subscriber.go
@@ -18,7 +18,10 @@ func (cs *ConcreteSubscriber) Notify(msg Message) {
 		fmt.Printf("Subscriber %s received message: %v\n", cs.ID, msg)
 	} else {
 		payload, _ := json.Marshal(msg)
-		http.Post(cs.Listener, "application/json", bytes.NewBuffer(payload))
+		_, err := http.Post(cs.Listener, "application/json", bytes.NewBuffer(payload))
+		if err != nil {
+			fmt.Printf("Failed to notify subscriber %s: %v\n", cs.ID, err)
+		}
 	}
 }
 
