4ed1fb1cb1fcafcd009a502031012be36330c1c4
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2024-08-16 11:53:29 +0200

    [Feat.] Use go routines to Notify

--- internal/messagebroker/broker.go
+++ internal/messagebroker/broker.go
@@ -26,26 +26,30 @@ func NewBroker() *Broker {
 	}
 }
 
+// Subscribe registers a subscriber to a topic
 func (b *Broker) Subscribe(topic string, subscriber Subscriber) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	b.subscribers[topic] = append(b.subscribers[topic], subscriber)
 }
 
+// Publish sends a message to all subscribers of a topic concurrently
 func (b *Broker) Publish(msg Message) {
 	b.mu.RLock()
 	defer b.mu.RUnlock()
 	for _, subscriber := range b.subscribers[msg.Topic] {
-		subscriber.Notify(msg)
+		go subscriber.Notify(msg) // Notify each subscriber in a separate Go routine
 	}
 }
 
+// RegisterPublisher adds a publisher to the broker
 func (b *Broker) RegisterPublisher(id string) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	b.publishers[id] = true
 }
 
+// IsPublisherRegistered checks if a publisher is registered
 func (b *Broker) IsPublisherRegistered(id string) bool {
 	b.mu.RLock()
 	defer b.mu.RUnlock()
